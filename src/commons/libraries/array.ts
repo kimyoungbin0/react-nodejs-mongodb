b266cc2d7efa94f0801248da5647165aa44b3f7b
cooldevsmart <cooldevsmart@gmail.com>
2023-02-24 14:22:34 +0900

    reduceArray 오류 수정

--- src/commons/libraries/array.ts
+++ src/commons/libraries/array.ts
@@ -1,8 +1,11 @@
 import _ from "lodash";
 
 export const reduceArray = (arr: any[], n: number) => {
-  if (n <= 1) {
-    return arr;
+  if (arr !== undefined && arr.length > 0) {
+    if (n <= 1) {
+      return arr;
+    }
+    return _.filter(arr, (_, i) => (i + 1) % n === 0);
   }
-  return _.filter(arr, (_, i) => (i + 1) % n === 0);
+  return [];
 };
